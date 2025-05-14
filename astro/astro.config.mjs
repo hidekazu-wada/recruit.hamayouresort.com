7802ebdf4d2663bb497ee634d82cb1007e43328b
Hidekazu Wada <s.hazushi@gmail.com>
2025-05-14 11:03:41 +0900

    エイリアスを追加し、インポートパスを簡素化しました。これにより、コードの可読性が向上し、ファイルの参照が容易になります。

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import path from 'path';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: {
+        '@styles': path.resolve('./src/styles'),
+        '@components': path.resolve('./src/components'),
+        '@assets': path.resolve('./src/assets'),
+        '@layouts': path.resolve('./src/layouts'),
+      },
+    },
+  },
+});
