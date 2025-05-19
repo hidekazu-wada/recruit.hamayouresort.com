5c46950a4199f4039f6f7ba01611ee944c637de1
Hidekazu Wada <s.hazushi@gmail.com>
2025-05-19 12:39:04 +0900

    jobsモーダル実装前

--- astro/src/data/cultureData.ts
+++ astro/src/data/cultureData.ts
@@ -1,6 +1,6 @@
-import Culture01 from '@assets/images/culture/img-culture-01.png';
-import Culture02 from '@assets/images/culture/img-culture-02.png';
-import Culture03 from '@assets/images/culture/img-culture-03.png';
+import Culture01 from '@assets/images/culture/culturepage-equipment-01.png';
+import Culture02 from '@assets/images/culture/culturepage-equipment-02.png';
+import Culture03 from '@assets/images/culture/culturepage-equipment-03.png';
 // 必要に応じて他の画像もインポート
 
 export interface CultureItem {
